d393770504cf301bf106b5a07091dfa4407cb73b
SDargarh <sdargarh26@gmail.com>
2024-04-17 18:59:33 +0530

    added deployemnt script

---
+++ scripts/deploy.js
@@ -0,0 +1,64 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+// const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+async function main() {
+  [buyer, seller, inspector, lender] = await ethers.getSigners()
+
+  // Deploy Real Estate
+  const RealEstate = await ethers.getContractFactory('RealEstate')
+  const realEstate = await RealEstate.deploy()
+  await realEstate.deployed()
+
+  console.log(`Deployed Real Estate Contrat at: ${realEstate.address}`);
+
+  for(let i = 0; i<3; i++) {
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/${i+1}.png`)
+    await transaction.wait()
+  }
+
+  const Escrow = await ethers.getContractFactory('Escrow')
+  const escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  )
+  await escrow.deployed()
+
+  // Approve Property
+  for(let i = 0; i<3; i++) {
+    transaction = await realEstate.connect(seller).approve(escrow.address, i+1)
+    await transaction.wait()
+  }
+
+  // List Property
+  transaction = await escrow.connect(seller).list(1, tokens(20), tokens(10))
+  await transaction.wait()
+
+  // List Property
+  transaction = await escrow.connect(seller).list(2, tokens(15), tokens(5))
+  await transaction.wait()
+
+  // List Property
+  transaction = await escrow.connect(seller).list(3, tokens(10), tokens(5))
+  await transaction.wait()
+
+  console.log('finished');
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
