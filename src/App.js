4444eef22cd5550b898b9589be4b781b8d4d3643
SDargarh <sdargarh26@gmail.com>
2024-04-19 23:49:37 +0530

    added navigation component

--- src/App.js
+++ src/App.js
@@ -15,10 +15,16 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const accounts = window.ethereum.request({method : 'eth_requestAccounts'})
-    console.log(accounts);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({method : 'eth_requestAccounts'})
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
+
   }
 
   useEffect(()=>{
@@ -27,7 +33,7 @@ function App() {
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount}></Navigation>
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
