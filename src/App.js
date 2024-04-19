2c4553a96618008956bb43480cbee42cd09ad183
SDargarh <sdargarh26@gmail.com>
2024-04-19 18:38:49 +0530

    initial front end setup

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+// Components
+import Navigation from './components/Navigation';
+import Search from './components/Search';
+import Home from './components/Home';
+
+// ABIs
+import RealEstate from './abis/RealEstate.json'
+import Escrow from './abis/Escrow.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = window.ethereum.request({method : 'eth_requestAccounts'})
+    console.log(accounts);
+  }
+
+  useEffect(()=>{
+    loadBlockchainData()
+  }, [])
+
+  return (
+    <div>
+
+      <div className='cards__section'>
+
+        <h3>Welcome to Millow</h3>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
